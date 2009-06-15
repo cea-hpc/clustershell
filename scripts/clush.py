c04ff230932ba959facd403dcbc3a789a94bee75
Stephane Thiell <stephane.thiell@cea.fr>
2009-06-15 14:46:22 +0000

    clush script interactive mode improvements: - launch task in a separate thread, main thread handles blocking tty input using raw_input() - during run, hit 'enter' to get the current state of workers (identical to pdsh ^C) - interactive mode supports readline history features - minimalistic nodes management within the clush interactive shell (+nodes, -nodes)

--- scripts/clush.py
+++ scripts/clush.py
@@ -27,20 +27,23 @@ Pdsh-like with integrated dshbak command using the ClusterShell library.
 
 import fcntl
 import os
-import readline
 import sys
 import signal
+import thread
 from optparse import OptionParser
 
 sys.path.insert(0, '../lib')
 from ClusterShell.Event import EventHandler
 from ClusterShell.NodeSet import NodeSet
-from ClusterShell.Task import task_self
+from ClusterShell.Task import Task, task_self, task_wait
 from ClusterShell.Worker.Worker import WorkerSimple
 from ClusterShell import version
 
+class UpdatePromptException(Exception):
+    """Exception used by the signal handler"""
 
 class StdInputHandler(EventHandler):
+    """Standard input event handler class."""
     def __init__(self, worker):
         self.master_worker = worker
     def ev_read(self, worker):
@@ -48,7 +51,8 @@ class StdInputHandler(EventHandler):
     def ev_close(self, worker):
         self.master_worker.set_write_eof()
 
-class OutputHandler(EventHandler):
+class DirectOutputHandler(EventHandler):
+    """Direct output event handler class."""
     def __init__(self, label):
         self._label = label
     def ev_read(self, worker):
@@ -63,34 +67,118 @@ class OutputHandler(EventHandler):
             print "clush: %s: exited with exit code %d" % (ns, rc) 
     def ev_timeout(self, worker):
         print "clush: %s: command timeout" % worker.last_node()
-        
+    def ev_close(self, worker):
+        # Notify main thread to update its prompt
+        worker.task.set_info("USER_running", False)
+        if worker.task.info("USER_handle_SIGHUP"):
+            os.kill(os.getpid(), signal.SIGHUP)
 
-def display_buffers(worker):
+class GatherOutputHandler(EventHandler):
+    """Gathered output event handler class."""
+    def ev_close(self, worker):
+        # Display command output, try to order buffers by rc
+        for rc, nodeset in worker.iter_retcodes():
+            for buffer, nodeset in worker.iter_buffers(nodeset):
+                print "-" * 15
+                print NodeSet.fromlist(nodeset, autostep=3)
+                print "-" * 15
+                print buffer
 
-    # Display command output, try to order buffers by rc
-    for rc, nodeset in worker.iter_retcodes():
-        for buffer, nodeset in worker.iter_buffers(nodeset):
-            print "-" * 15
-            print NodeSet.fromlist(nodeset, autostep=3)
-            print "-" * 15
-            print buffer
+        # Display return code if not ok ( != 0)
+        for rc, nodeset in worker.iter_retcodes():
+            if rc != 0:
+                ns = NodeSet.fromlist(nodeset, autostep=3)
+                print "clush: %s: exited with exit code %s" % (ns, rc)
 
-    # Display return code if not ok ( != 0)
-    for rc, nodeset in worker.iter_retcodes():
-        if rc != 0:
-            ns = NodeSet.fromlist(nodeset, autostep=3)
-            print "clush: %s: exited with exit code %s" % (ns, rc)
+        # Notify main thread to update its prompt
+        worker.task.set_info("USER_running", False)
+        if worker.task.info("USER_handle_SIGHUP"):
+            os.kill(os.getpid(), signal.SIGHUP)
+
+def signal_handler(signum, frame):
+    """Signal handler used for main thread notification"""
+    if signum == signal.SIGHUP:
+        raise UpdatePromptException()
+
+def get_history_file():
+    """Turn the history file path"""
+    return os.path.join(os.environ["HOME"], ".clush_history")
+
+def readline_setup():
+    """
+    Configure readline to automatically load and save a history file
+    named .clush_history
+    """
+    import readline
+    try:
+        readline.read_history_file(get_history_file())
+    except IOError:
+        pass
+
+def ttyloop(task, nodeset, gather, timeout, label):
+    """Manage the interactive prompt to run command"""
+    if task.info("USER_interactive"):
+        import readline
+        assert sys.stdin.isatty()
+        readline_setup()
+        print "Enter 'quit' to leave this interactive mode"
+
+    rc = 0
+    ns = NodeSet(nodeset)
+    cmd = ""
+    while task.info("USER_running") or cmd.lower() != 'quit':
+        try:
+            if task.info("USER_interactive") and not task.info("USER_running"):
+                print "Working with nodes: %s" % ns
+                prompt = "clush> "
+            else:
+                prompt = ""
+            cmd = raw_input(prompt)
+        except EOFError:
+            return
+        except UpdatePromptException:
+            if task.info("USER_interactive"):
+                continue
+            return
+        if task.info("USER_running"):
+            ns_reg, ns_unreg = NodeSet(), NodeSet()
+            for c in task._engine.clients():
+                if c.registered:
+                    ns_reg.add(c.key)
+                else:
+                    ns_unreg.add(c.key)
+            if ns_unreg:
+                pending = "\nclush: pending(%d): %s" % (len(ns_unreg), ns_unreg)
+            else:
+                pending = ""
+            print >>sys.stderr, "clush: interrupt (^C to abort task)\n" \
+                    "clush: in progress(%d): %s%s" % (len(ns_reg), ns_reg, pending)
+        else:
+            cmdl = cmd.lower()
+            if cmdl.startswith('+'):
+                ns.add(cmdl[1:])
+            elif cmdl.startswith('-'):
+                ns.remove(cmdl[1:])
+            elif cmdl.startswith('='):
+                ns = NodeSet(cmdl[1:])
+            elif cmdl != "quit":
+                if not cmd:
+                    continue
+                readline.write_history_file(get_history_file())
+                run_command(task, cmd, ns, gather, timeout, label)
+    return rc
 
 def run_command(task, cmd, ns, gather, timeout, label):
     """
     Create and run the specified command line, displaying
     results in a dshbak way if gathering is used.
     """    
+    task.set_info("USER_running", True)
 
     if gather:
-        worker = task.shell(cmd, nodes=ns, timeout=timeout)
+        worker = task.shell(cmd, nodes=ns, handler=GatherOutputHandler(), timeout=timeout)
     else:
-        worker = task.shell(cmd, nodes=ns, handler=OutputHandler(label), timeout=timeout)
+        worker = task.shell(cmd, nodes=ns, handler=DirectOutputHandler(label), timeout=timeout)
 
     if not sys.stdin.isatty():
         # Switch stdin to non blocking mode
@@ -104,28 +192,6 @@ def run_command(task, cmd, ns, gather, timeout, label):
         task.schedule(worker_stdin)
  
     task.resume()
-
-    if gather:
-       display_buffers(worker)
-   
-    return task.max_retcode()
-
-def interactive(task, ns, gather, timeout, label):
-   """Manage the interactive prompt to run command"""
-   rc = 0
-   cmd = ""
-   print "Enter 'quit' to leave this interactive mode"
-   while cmd.lower() != "quit":
-        try:
-            cmd = raw_input("clush> ")
-        except EOFError:
-            print
-            break
-
-        if cmd.lower() != "quit":
-            rc = run_command(task, cmd, ns, gather, timeout, label)
-
-   return rc
 
 def clush_main(args):
     """Main clush script function"""
@@ -183,8 +249,21 @@ def clush_main(args):
     #
     # Task management
     #
+    stdin_isatty = sys.stdin.isatty()
+    if stdin_isatty:
+        # Standard input is a terminal and we want to perform some user
+        # interactions in the main thread (using blocking calls), so
+        # we run cluster commands in a new ClusterShell Task (a new
+        # thread is created).
+        task = Task()
+        signal.signal(signal.SIGHUP, signal_handler)
+        task.set_info("USER_handle_SIGHUP", True)
+    else:
+        # Perform everything in main thread.
+        task = task_self()
+        task.set_info("USER_handle_SIGHUP", False)
+
     timeout = 0
-    task = task_self()
     if options.debug:
         task.set_info("debug", options.debug)
     if options.fanout:
@@ -197,26 +276,32 @@ def clush_main(args):
     if options.command_timeout:
         task.set_info("command_timeout", options.command_timeout)
         timeout += options.command_timeout
+            
+    # Configure custom task related status
+    task.set_info("USER_interactive", len(args) == 0)
+    task.set_info("USER_running", False)
 
-    # Either we have no more arguments, so use interactive mode
-    if not len(args):
-        rc = interactive(task, nodeset_base, options.gather, timeout, options.label)
+    if not task.info("USER_interactive"):
+        run_command(task, ' '.join(args), nodeset_base, options.gather, timeout, options.label)
 
-    # If not, just prepare a command with the last args and run it
-    else:
-        rc = run_command(task, ' '.join(args), nodeset_base, options.gather, timeout, options.label)
+    if stdin_isatty:
+        ttyloop(task, nodeset_base, options.gather, timeout, options.label)
+    elif task.info("USER_interactive"):
+        print >>sys.stderr, "ERROR: interactive mode requires a tty"
+        os_.exit(1)
 
     # return the command retcode
     if options.maxrc:
-        sys.exit(rc)
+        os._exit(task.max_retcode())
     # return clush retcode
     else:
-        sys.exit(0)
+        os._exit(0)
 
 if __name__ == '__main__':
     try:
         clush_main(sys.argv)
     except KeyboardInterrupt:
         print "Keyboard interrupt."
-        sys.exit(128 + signal.SIGINT)
+        # Use os._exit to avoid threads cleanup
+        os._exit(128 + signal.SIGINT)
 
