7b38b06e9a7f8a5d361bb8bdadcdc611b24648a2
Stephane Thiell <stephane.thiell@cea.fr>
2009-07-30 22:23:45 +0000

    Merge correctly code coverage improvements in tests (#32)

--- tests/TaskDistantTest.py
+++ tests/TaskDistantTest.py
@@ -305,6 +305,32 @@ class TaskDistantTest(unittest.TestCase):
         # read result
         self.assertEqual(worker.node_buffer("localhost"), "foobar")
 
+    def testEscapePdsh(self):
+        """test distant worker (pdsh) cmd with escaped variable"""
+        task = task_self()
+        self.assert_(task != None)
+        worker = WorkerPdsh("localhost", command="export CSTEST=foobar; /bin/echo \$CSTEST | sed 's/\ foo/bar/'",
+                handler=None, timeout=None)
+        self.assert_(worker != None)
+        #task.set_info("debug", True)
+        task.schedule(worker)
+        # execute
+        task.resume()
+        # read result
+        self.assertEqual(worker.node_buffer("localhost"), "$CSTEST")
+
+    def testEscapePdsh2(self):
+        """test distant worker (pdsh) cmd with non-escaped variable"""
+        task = task_self()
+        self.assert_(task != None)
+        worker = WorkerPdsh("localhost", command="export CSTEST=foobar; /bin/echo $CSTEST | sed 's/\ foo/bar/'",
+                handler=None, timeout=None)
+        task.schedule(worker)
+        # execute
+        task.resume()
+        # read result
+        self.assertEqual(worker.node_buffer("localhost"), "foobar")
+
     def testWorkerBuffers(self):
         """test buffers at worker level"""
         task = task_self()
@@ -322,34 +348,6 @@ class TaskDistantTest(unittest.TestCase):
 
         self.assertEqual(cnt, 0)
 
-    #
-    # FIXME: Uncomment as soon as ticket #26 is fixed.
-    #
-    #def testEscapePdsh(self):
-    #    """test distant worker (pdsh) cmd with escaped variable"""
-    #    task = task_self()
-    #    self.assert_(task != None)
-    #    worker = WorkerPdsh("localhost", command="export CSTEST=foobar; /bin/echo \$CSTEST | sed 's/\ foo/bar/'",
-    #            handler=None, timeout=None)
-    #    self.assert_(worker != None)
-    #    task.set_info("debug", True)
-    #    task.schedule(worker)
-    #    # execute
-    #    task.resume()
-    #    # read result
-    #    self.assertEqual(worker.node_buffer("localhost"), "$CSTEST")
-
-    #def testEscapePdsh2(self):
-    #    """test distant worker (pdsh) cmd with non-escaped variable"""
-    #    task = task_self()
-    #    self.assert_(task != None)
-    #    worker = WorkerPdsh("localhost", command="export CSTEST=foobar; /bin/echo $CSTEST | sed 's/\ foo/bar/'",
-    #            handler=None, timeout=None)
-    #    task.schedule(worker)
-    #    # execute
-    #    task.resume()
-    #    # read result
-    #    self.assertEqual(worker.node_buffer("localhost"), "foobar")
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TaskDistantTest)
