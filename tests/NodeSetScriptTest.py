61c5e67b1e3a324eb16edfad3ee086992579301a
Aurelien Degremont <aurelien.degremont@cea.fr>
2009-07-31 11:25:10 +0000

    Add non-reg tests for nodeset.py (--rangeset, --exclude and stdin)

--- tests/NodeSetScriptTest.py
+++ tests/NodeSetScriptTest.py
@@ -16,8 +16,8 @@ from subprocess import *
 class NodeSetScriptTest(unittest.TestCase):
     """Unit test class for testing nodeset.py"""
 
-    def _launchAndCompare(self, args, expected_output):
-        output = Popen(["../scripts/nodeset.py"] + args, stdout=PIPE).communicate()[0].strip()
+    def _launchAndCompare(self, args, expected_output, stdin=None):
+        output = Popen(["../scripts/nodeset.py"] + args, stdout=PIPE, stdin=PIPE).communicate(input=stdin)[0].strip()
         if type(expected_output) is list:
             ok = False
             for o in expected_output:
@@ -117,7 +117,29 @@ class NodeSetScriptTest(unittest.TestCase):
         self._launchAndCompare(["-X", "--fold", "foo[395-442]", "foo", "foo[0-200,245-394]"], ["foo[0-200,245-442],foo", "foo,foo[0-200,245-442]"])
         self._launchAndCompare(["-X", "--fold", "foo[395-442]", "bar3,bar24", "foo[1-200,245-394]"], ["foo[1-200,245-442],bar[3,24]", "bar[3,24],foo[1-200,245-442]"])
 
+    def testExclude(self):
+        """test nodeset.py --fold --exclude"""
+        # Empty result
+        self._launchAndCompare(["--fold","-x", "foo", "foo"], "")
+        # With no range
+        self._launchAndCompare(["--fold","-x", "foo", "foo,bar"], "bar")
+        # Normal with range
+        self._launchAndCompare(["--fold","-x", "foo[0-5]", "foo[0-10]"], "foo[6-10]")
+        # Do no change
+        self._launchAndCompare(["--fold","-x", "bar[0-5]", "foo[6-10]"], "foo[6-10]")
+        self._launchAndCompare(["--fold","--exclude", "foo[5-10,15]", "foo[0-10]", "foo[13-18]"], "foo[0-4,13-14,16-18]")
 
+    def testRangeSet(self):
+        """test nodeset.py --rangeset"""
+        self._launchAndCompare(["--fold","--rangeset","1,2"], "1-2")
+        self._launchAndCompare(["--expand","-R","1-2"], "1 2")
+        self._launchAndCompare(["--fold","-X","-R","1-2","2-3"], "1,3")
+
+    def testStdin(self):
+        """test nodeset.py - (stdin)"""
+        self._launchAndCompare(["-f","-"], "foo", stdin="foo\n")
+        self._launchAndCompare(["-f","-"], "foo[1-3]", stdin="foo1 foo2 foo3\n")
+        
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(NodeSetScriptTest)
     unittest.TextTestRunner(verbosity=2).run(suite)
