96b1a54194f8fbdfbe74e944ff986675115e074c
Stephane Thiell <stephane.thiell@cea.fr>
2009-08-07 14:24:52 +0000

    Update version and changelog for release 1.1 RC6.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if not os.access('scripts/nodeset', os.F_OK):
     os.symlink('nodeset.py', 'scripts/nodeset')
 
 setup(name='ClusterShell',
-      version='1.0.95',
+      version='1.0.96',
       license='CeCILL-C',
       description='ClusterShell library',
       author='Stephane Thiell',
