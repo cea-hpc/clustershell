53467590299b334c6fd984c288c00c7d3dde1894
Stephane Thiell <stephane.thiell@cea.fr>
2010-05-24 22:43:11 +0000

    Fix trac #78 (clubak: add regroup (-r) support).

--- scripts/clubak.py
+++ scripts/clubak.py
@@ -45,17 +45,35 @@ import optparse
 import signal
 import sys
 
-from ClusterShell.MsgTree import MsgTree
-from ClusterShell.NodeSet import NodeSet, NodeSetParseError
-from ClusterShell import __version__
+from ClusterShell.NodeUtils import GroupResolverConfigError
+from ClusterShell.NodeUtils import GroupResolverSourceError
+from ClusterShell.NodeUtils import GroupSourceException
+from ClusterShell.NodeUtils import GroupSourceNoUpcall
+try:
+    from ClusterShell.MsgTree import MsgTree
+    from ClusterShell.NodeSet import NodeSet
+    from ClusterShell.NodeSet import NodeSetExternalError, NodeSetParseError
+    from ClusterShell import __version__
+except GroupResolverConfigError, e:
+    print >> sys.stderr, \
+        "ERROR: ClusterShell Groups configuration error:\n\t%s" % e
+    sys.exit(1)
 
-def print_buffer(header, content):
+def print_buffer(nodeset, content, regroup, groupsource):
     """Display a dshbak-like header block and content."""
     sep = "-" * 15
+    if regroup:
+        header = nodeset.regroup(groupsource)
+    else:
+        header = str(nodeset)
     sys.stdout.write("%s\n%s\n%s\n%s\n" % (sep, header, sep, content))
 
-def print_lines(header, msg):
+def print_lines(nodeset, msg, regroup, groupsource):
     """Display a MsgTree buffer by line with prefixed header."""
+    if regroup:
+        header = nodeset.regroup(groupsource)
+    else:
+        header = str(nodeset)
     for line in msg:
         sys.stdout.write("%s: %s\n" % (header, line))
 
@@ -71,7 +89,7 @@ def nodeset_cmp(ns1, ns2):
             return 1
     return len_cmp
 
-def display(tree, line_mode, gather):
+def display(tree, line_mode, gather, regroup, groupsource):
     """Display results"""
     try:
         if gather:
@@ -81,19 +99,21 @@ def display(tree, line_mode, gather):
             for nodeset in sorted(imap(ns_getter, tree.walk()),
                                   cmp=nodeset_cmp):
                 if line_mode:
-                    print_lines(nodeset, tree[nodeset[0]])
+                    print_lines(nodeset, tree[nodeset[0]], regroup,
+                                groupsource)
                 else:
-                    print_buffer(NodeSet.fromlist(nodeset), tree[nodeset[0]])
+                    print_buffer(NodeSet.fromlist(nodeset), tree[nodeset[0]],
+                                 regroup, groupsource)
         else:
             # automagically sorted by NodeSet
             nodes = NodeSet.fromlist(tree.keys())
 
             if line_mode:
                 for node in nodes:
-                    print_lines(node, tree[node])
+                    print_lines(node, tree[node], regroup, groupsource)
             else:
                 for node in nodes:
-                    print_buffer(node, tree[node])
+                    print_buffer(node, tree[node], regroup, groupsource)
     finally:
         sys.stdout.flush()
 
@@ -109,12 +129,16 @@ def clubak():
     parser.disable_interspersed_args()
 
     parser.add_option("-b", "-c", action="store_true", dest="gather",
-                      help="gather nodes with same output (-c is provided for " \
-                           "dshbak compatibility)")
+                      help="gather nodes with same output (-c is provided " \
+                           "for dshbak compatibility)")
     parser.add_option("-d", "--debug", action="store_true", dest="debug",
                       help="output more messages for debugging purpose")
     parser.add_option("-L", action="store_true", dest="line_mode",
                       help="disable header block and order output by nodes")
+    parser.add_option("-r", "--regroup", action="store_true", dest="regroup",
+                      default=False, help="fold nodeset using node groups")
+    parser.add_option("-s", "--groupsource", action="store", dest="groupsource",
+                      help="optional groups.conf(5) group source to use")
     options = parser.parse_args()[0]
 
     # Create new message tree
@@ -129,15 +153,28 @@ def clubak():
     if options.debug:
         print >> sys.stderr, "clubak: line_mode=%s gather=%s tree_depth=%d" % \
             (bool(options.line_mode), bool(options.gather), tree._depth())
-    display(tree, options.line_mode, options.gather)
-
+    display(tree, options.line_mode, options.gather, options.regroup,
+            options.groupsource)
+    sys.exit(0)
 
 if __name__ == '__main__':
     try:
         clubak()
-        sys.exit(0)
+    except NodeSetExternalError, e:
+        print >> sys.stderr, "clubak: external error:", e
+        sys.exit(1)
     except NodeSetParseError, e:
         print >> sys.stderr, "clubak: parse error:", e
+        sys.exit(1)
+    except GroupResolverSourceError, e:
+        print >> sys.stderr, "ERROR: unknown group source: \"%s\"" % e
+        sys.exit(1)
+    except GroupSourceNoUpcall, e:
+        print >> sys.stderr, "ERROR: no %s upcall defined for group " \
+            "source \"%s\"" % (e, e.group_source.name)
+        sys.exit(1)
+    except GroupSourceException, e:
+        print >> sys.stderr, "ERROR: other group error:", e
         sys.exit(1)
     except IOError:
         sys.exit(1)     # exit with error on broken pipe
