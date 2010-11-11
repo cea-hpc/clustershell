c1f0734518e0a3d4118ee9225294ab6d363faefe
Stephane Thiell <stephane.thiell@cea.fr>
2010-11-11 17:12:17 +0000

    mkrpm.sh fixes: add license header and install commands for CLI package

--- mkrpm.sh
+++ mkrpm.sh
@@ -1,8 +1,41 @@
 #!/bin/sh
+#
+# Copyright CEA/DAM/DIF (2008, 2009, 2010)
+#  Contributor: Stephane THIELL <stephane.thiell@cea.fr>
+#
+# This file is part of the ClusterShell library. 
+#
+# This software is governed by the CeCILL-C license under French law and
+# abiding by the rules of distribution of free software.  You can  use, 
+# modify and/ or redistribute the software under the terms of the CeCILL-C
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info". 
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability. 
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or 
+# data to be ensured and,  more generally, to use and operate it in the 
+# same conditions as regards security. 
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL-C license and that you accept its terms.
+#
 # $Id$
 
 if [ -z "$2" ]; then
-    echo "usage: $0 <version> <el5|fc11>"
+    echo "usage: $0 <version> <dist>"
+    echo "example: $0 1.2.3 el6"
     exit 1
 fi
 
@@ -18,6 +51,7 @@ rm -rf $TMPROOT
 echo "Building tarball..."
 
 install -d $TMPDIR/lib/ClusterShell || exit 1
+install -d $TMPDIR/lib/ClusterShell/CLI
 install -d $TMPDIR/lib/ClusterShell/Engine
 install -d $TMPDIR/lib/ClusterShell/Worker
 install -d $TMPDIR/scripts
@@ -30,6 +64,7 @@ install -d $TMPDIR/tests
 install -p -m 0644 setup.cfg setup.py $TMPDIR/
 install -p -m 0644 README ChangeLog Licence_CeCILL-C_V1-en.txt Licence_CeCILL-C_V1-fr.txt $TMPDIR/
 install -p -m 0644 lib/ClusterShell/*.py $TMPDIR/lib/ClusterShell
+install -p -m 0644 lib/ClusterShell/CLI/*.py $TMPDIR/lib/ClusterShell/CLI/
 install -p -m 0644 lib/ClusterShell/Engine/*.py $TMPDIR/lib/ClusterShell/Engine/
 install -p -m 0644 lib/ClusterShell/Worker/*.py $TMPDIR/lib/ClusterShell/Worker/
 install -p -m 0755 scripts/clubak.py $TMPDIR/scripts/
