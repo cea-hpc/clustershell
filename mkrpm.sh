2bfd54226f3d931b1a6bd7776299a733d17ce997
Stephane Thiell <stephane.thiell@cea.fr>
2015-04-10 00:09:38 +0200

    Tree: added conf/topology.conf.example
    Also added clush --topology=FILE option in man page.
    
    Change-Id: I76e0cc19ecf09946eb138c517a154a71b6c9d9ba

--- mkrpm.sh
+++ mkrpm.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 #
-# Copyright CEA/DAM/DIF (2008, 2009, 2010, 2011, 2012)
+# Copyright CEA/DAM/DIF (2008-2015)
 #  Contributor: Stephane THIELL <stephane.thiell@cea.fr>
 #
-# This file is part of the ClusterShell library. 
+# This file is part of the ClusterShell library.
 #
 # This software is governed by the CeCILL-C license under French law and
-# abiding by the rules of distribution of free software.  You can  use, 
+# abiding by the rules of distribution of free software.  You can  use,
 # modify and/ or redistribute the software under the terms of the CeCILL-C
 # license as circulated by CEA, CNRS and INRIA at the following URL
-# "http://www.cecill.info". 
+# "http://www.cecill.info".
 #
 # As a counterpart to the access to the source code and  rights to copy,
 # modify and redistribute granted by the license, users are provided only
 # with a limited warranty  and the software's author,  the holder of the
 # economic rights,  and the successive licensors  have only  limited
-# liability. 
+# liability.
 #
 # In this respect, the user's attention is drawn to the risks associated
 # with loading,  using,  modifying and/or developing or reproducing the
@@ -24,9 +24,9 @@
 # therefore means  that it is reserved for developers  and  experienced
 # professionals having in-depth computer knowledge. Users are therefore
 # encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
-# same conditions as regards security. 
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
 #
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL-C license and that you accept its terms.
@@ -75,6 +75,7 @@ install -p -m 0644 conf/clush.conf $TMPDIR/conf/
 install -p -m 0644 conf/groups conf/groups.conf $TMPDIR/conf/
 install -p -m 0644 conf/groups.conf.d/README $TMPDIR/conf/groups.conf.d/
 install -p -m 0644 conf/groups.conf.d/*.conf.example $TMPDIR/conf/groups.conf.d/
+install -p -m 0644 conf/topology.conf.example $TMPDIR/conf/
 install -p -m 0644 doc/epydoc/clustershell_epydoc.conf $TMPDIR/doc/epydoc/
 install -p -m 0755 doc/examples/*.py $TMPDIR/doc/examples/
 install -p -m 0644 doc/extras/vim/ftdetect/clustershell.vim $TMPDIR/doc/extras/vim/ftdetect/
