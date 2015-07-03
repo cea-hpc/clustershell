cea-hpc/clustershell

commit ecbb2b615b8eb19799604d35ee07d1b1ca4e4c58

NodeSet: fix possible internal wrong group resolver pickup
Specifying the group resolver in NodeSet constructor could still lead
to the standard group resolver being used internally.

Improve tests to ensure that no local group config are ever used to
allow this kind of error detection.

lib/ClusterShell/NodeSet.py
@@ -1149,7 +1149,8 @@ class NodeSet(NodeSetBase):
                 # use internal reverse: populate allgroups
                 for grp in allgrplist:
                     nodelist = self._resolver.group_nodes(grp, groupsource)
-                    allgroups[grp] = NodeSet(",".join(nodelist))
+                    allgroups[grp] = NodeSet(",".join(nodelist), \
+                                             resolver=RESOLVER_NOGROUP)
             except NodeUtils.GroupSourceQueryFailed, exc:
                 # External result inconsistency
                 raise NodeSetExternalError("Unable to map a group " \
@@ -1182,7 +1183,8 @@ class NodeSet(NodeSetBase):
                 key = "@%s:%s" % (groupsource, grp)
             else:
                 key = "@" + grp
-            result[key] = (NodeSet(nsb), self.intersection(nsb))
+            result[key] = (NodeSet(nsb, resolver=RESOLVER_NOGROUP), \
+                           self.intersection(nsb))
         return result
 
     def regroup(self, groupsource=None, autostep=None, overlap=False,

tests/NodeSetGroupTest.py
@@ -129,6 +129,15 @@ rack-all: @rack-x[1-2]y[1-2]
 
 class NodeSetGroupTest(unittest.TestCase):
 
+    def setUp(self):
+        """setUp test reproducibility: change standard group resolver
+        to ensure that no local group source is used during tests"""
+        set_std_group_resolver(GroupResolver()) # dummy resolver
+
+    def tearDown(self):
+        """tearDown: restore standard group resolver"""
+        set_std_group_resolver(None) # restore std resolver
+
     def testGroupResolverSimple(self):
         """test NodeSet with simple custom GroupResolver"""
 
@@ -912,6 +921,15 @@ class NodeSetGroup2GSTest(unittest.TestCase):
 
 
 class NodeSetRegroupTest(unittest.TestCase):
+
+    def setUp(self):
+        """setUp test reproducibility: change standard group resolver
+        to ensure that no local group source is used during tests"""
+        set_std_group_resolver(GroupResolver()) # dummy resolver
+
+    def tearDown(self):
+        """tearDown: restore standard group resolver"""
+        set_std_group_resolver(None) # restore std resolver
 
     def testGroupResolverReverse(self):
         """test NodeSet GroupResolver with reverse upcall"""

commit 2491d7ad5059a07ececf1f8b80467ae64b16908f

NodeSet: restore nested group resolution support
Commit ecbb2b615b8eb19799604d35ee07d1b1ca4e4c58 introduced a wrong fix
leading to broken nested group resolution support. Worst, it introduced
a wrong test (with nested groups).,

This patch addresses the original issue, that is, inherit group resolver
when instantiating NodeSet object internally that needs group support,
but without breaking nested group resolution support.

To solve this resolver inheritance issue, NodeSet constructor now
support resolver=nodeset object as parameter. Used this way, it will
pick nodeset's resolver as new NodeSet object resolver.

Change-Id: If9919e8866a41e70824e732e11d8dc472bc2be09

lib/ClusterShell/NodeSet.py
@@ -1026,10 +1026,15 @@ class NodeSet(NodeSetBase):
 
         self._autostep = autostep
 
-        # Set group resolver.
+        # Set group resolver - several use cases apply:
         if resolver in (RESOLVER_NOGROUP, RESOLVER_NOINIT):
+            # Special constants to avoid any resolver binding
             self._resolver = None
+        elif hasattr(resolver, '_resolver'):
+            # Inherit _resolver from object (eg. NodeSet object)
+            self._resolver = resolver._resolver
         else:
+            # Pass a resolver instance or None to default to standard one
             self._resolver = resolver or RESOLVER_STD_GROUP
 
         # Initialize default parser.
@@ -1152,8 +1157,7 @@ class NodeSet(NodeSetBase):
                 # use internal reverse: populate allgroups
                 for grp in allgrplist:
                     nodelist = self._resolver.group_nodes(grp, groupsource)
-                    allgroups[grp] = NodeSet(",".join(nodelist), \
-                                             resolver=RESOLVER_NOGROUP)
+                    allgroups[grp] = NodeSet(",".join(nodelist), resolver=self)
             except NodeUtils.GroupSourceQueryFailed, exc:
                 # External result inconsistency
                 raise NodeSetExternalError("Unable to map a group " \
@@ -1186,8 +1190,7 @@ class NodeSet(NodeSetBase):
                 key = "@%s:%s" % (groupsource, grp)
             else:
                 key = "@" + grp
-            result[key] = (NodeSet(nsb, resolver=RESOLVER_NOGROUP), \
-                           self.intersection(nsb))
+            result[key] = (NodeSet(nsb, resolver=self), self.intersection(nsb))
         return result
 
     def regroup(self, groupsource=None, autostep=None, overlap=False,

tests/NodeSetGroupTest.py
@@ -822,14 +822,31 @@ reverse: echo f^oo
         nodeset = NodeSet("@rack-all", resolver=res)
         self.assertEqual(str(nodeset), "idaho[1-7]z1")
 
-        # test nD groups()
-        self.assertEqual(sorted(nodeset.groups().keys()), ['@rack-x1y1', '@rack-x1y2', '@rack-x2y1', '@rack-x2y2'])
-        self.assertEqual(sorted(nodeset.groups(groupsource="simple").keys()), ['@simple:rack-x1y1', '@simple:rack-x1y2', '@simple:rack-x2y1', '@simple:rack-x2y2'])
-        self.assertEqual(sorted(nodeset.groups(groupsource="simple", noprefix=True).keys()), ['@rack-x1y1', '@rack-x1y2', '@rack-x2y1', '@rack-x2y2'])
+        # test NESTED nD groups()
+        self.assertEqual(sorted(nodeset.groups().keys()),
+                         ['@rack-all', '@rack-x1', '@rack-x1y1', '@rack-x1y2',
+                          '@rack-x2', '@rack-x2y1', '@rack-x2y2', '@rack-y1',
+                          '@rack-y2'])
+        self.assertEqual(sorted(nodeset.groups(groupsource="simple").keys()),
+                         ['@simple:rack-all', '@simple:rack-x1',
+                          '@simple:rack-x1y1', '@simple:rack-x1y2',
+                          '@simple:rack-x2', '@simple:rack-x2y1',
+                          '@simple:rack-x2y2', '@simple:rack-y1',
+                          '@simple:rack-y2'])
+        self.assertEqual(sorted(nodeset.groups(groupsource="simple",
+                                               noprefix=True).keys()),
+                         ['@rack-all', '@rack-x1', '@rack-x1y1', '@rack-x1y2',
+                          '@rack-x2', '@rack-x2y1', '@rack-x2y2', '@rack-y1',
+                          '@rack-y2'])
         testns = NodeSet()
         for gnodes, inodes in nodeset.groups().itervalues():
             testns.update(inodes)
         self.assertEqual(testns, nodeset)
+
+        # one more test with single node nodeset and nested groups
+        nodeset = NodeSet("idaho5z1", resolver=res)
+        self.assertEqual(sorted(nodeset.groups().keys()),
+                         ['@rack-all', '@rack-x2', '@rack-x2y1', '@rack-y1'])
 
 
 class NodeSetGroup2GSTest(unittest.TestCase):