87c0bbae0c3ea878cbfeaad72bdf3ef4fea6f468
Stephane Thiell <sthiell@stanford.edu>
2017-07-26 23:02:39 -0700

    CLI/Nodeset.py: random.sample() takes a sequence (#337)
    Fixes the following exception in Python 3:
    
    TypeError: Population must be a sequence or set.  For dicts, use list(d).
    
    Change-Id: I9bae5ab310805fd302d2bf150f7ef96a14f32529

--- lib/ClusterShell/CLI/Nodeset.py
+++ lib/ClusterShell/CLI/Nodeset.py
@@ -284,7 +284,7 @@ def nodeset():
     if options.pick and options.pick < len(xset):
         # convert to string for sample as nsiter() is slower for big
         # nodesets; and we assume options.pick will remain small-ish
-        keep = random.sample(xset, options.pick)
+        keep = random.sample(list(xset), options.pick)
         # explicit class_set creation and str() convertion for RangeSet
         keep = class_set(','.join([str(x) for x in keep]))
         xset.intersection_update(keep)
